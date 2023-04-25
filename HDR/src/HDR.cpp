ee7fe020079b10f502f8bc42e69b7dc37534fecc
Benjamin Beilharz <benjamin.beilharz@pm.me>
2023-04-25 23:07:20 +0200

    init HDR

---
+++ HDR/src/HDR.cpp
@@ -0,0 +1,13 @@
+﻿// HDR.cpp : Defines the entry point for the application.
+//
+
+#include "HDR.h"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ HDR/src/HDR.h
@@ -0,0 +1,9 @@
+﻿// HDR.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <opencv2/core.hpp>
+
+// TODO: Reference additional headers your program requires here.
